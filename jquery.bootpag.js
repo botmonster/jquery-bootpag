663c94f1ea2ee88c97c0c20207ca4ea4e4e79ff5
botmonster <przemek@7items.com>
2013-02-26 22:58:08 +0100

    update to 1.0.3

--- jquery.bootpag.js
+++ jquery.bootpag.js
@@ -1,120 +1,19 @@
 /*
- * bootpag - jQuery plugin for dynamic pagination
- *
- * Copyright (c) 2013 botmonster@7items.com
- *
- * Licensed under the MIT license:
- *   http://www.opensource.org/licenses/mit-license.php
- *
- * Project home:
- *   http://botmonster.github.com/jquery-bootpag
- *
- * Version:  1.0.2
- *
- */
-(function($, window) {
+ 
+ bootpag - jQuery plugin for dynamic pagination
 
-    $.fn.bootpag = function(options){
+ Copyright (c) 2013 botmonster@7items.com
 
-        var $owner = this, 
-            settings = $.extend({
-                total: 0,
-                page: 1,
-                maxVisible: null,
-                leaps: true,
-                href: 'javascript:void(0);',
-                hrefVariable: '{{number}}',
-                next: '&raquo;',
-                prev: '&laquo;'
-            }, 
-            $owner.settings || {}, 
-            options || {});
+ Licensed under the MIT license:
+   http://www.opensource.org/licenses/mit-license.php
 
-        if(settings.total <= 0)
-            return this;
+ Project home:
+   http://botmonster.com/jquery-bootpag/
 
-        if(!settings.maxVisible){
-            settings.maxVisible = settings.total;
-        }
+ Version:  1.0.3
 
-        $owner.settings = settings;
-
-        function renderPage($bootpag, page){
-        
-            var lp,
-                vis = Math.floor(page / settings.maxVisible) * settings.maxVisible,
-                $page = $bootpag.find('li');
-
-            $page.removeClass('disabled');
-            lp = page - 1 < 1 ? 1 : 
-                    settings.leaps && page - 1 > settings.maxVisible ? 
-                        Math.floor((page - 1) / settings.maxVisible) * settings.maxVisible : page - 1;
-            $page
-                .first()
-                .toggleClass('disabled', page === 1)
-                .attr('data-lp', lp)
-                .find('a').attr('href', href(lp));
-            lp = page + 1 > settings.total ? settings.total : 
-                    settings.leaps && page + 1 < settings.total - settings.maxVisible ? 
-                        vis + settings.maxVisible + 1: page + 1;
-            $page
-                .last()
-                .toggleClass('disabled', page === settings.total)
-                .attr('data-lp', lp)
-                .find('a').attr('href', href(lp));;
-
-            var $currPage = $page.filter('[data-lp='+page+']');
-            if(!$currPage.not('.next,.prev').length){
-                var d = page <= vis ? -settings.maxVisible : 0;
-                $page.not('.next,.prev').each(function(index){
-                    lp = index + 1 + vis + d;
-                    $(this)
-                        .attr('data-lp', lp)
-                        .toggle(lp <= settings.total)
-                        .find('a').html(lp).attr('href', href(lp));
-                });
-                $currPage = $page.filter('[data-lp='+page+']');
-            }
-            $currPage.addClass('disabled');
-            $owner.trigger('page', page);
-        }
-
-
-        function href(c){
-
-            return settings.href.replace(settings.hrefVariable, c);
-        }
-
-        return this.each(function(){
-
-            var $bootpag, lp, me = $(this),
-                p = ['<ul class="bootpag">'];
-
-            if(settings.prev){
-                p.push('<li data-lp="1" class="prev"><a href="'+href(1)+'">'+settings.prev+'</a></li>');
-            }
-            for(var c = 1; c <= Math.min(settings.total, settings.maxVisible); c++){
-                p.push('<li data-lp="'+c+'"><a href="'+href(c)+'">'+c+'</a></li>');
-            }
-            if(settings.next){
-                lp = settings.leaps && settings.total > settings.maxVisible
-                    ? Math.min(settings.maxVisible + 1, settings.total) : 2;
-                p.push('<li data-lp="'+lp+'" class="next"><a href="'+href(lp)+'">'+settings.next+'</a></li>');
-            }
-            p.push('</ul>');
-            me.find('ul.bootpag').remove();
-            me.append(p.join('')).addClass('pagination');
-            $bootpag = me.find('ul.bootpag');
-            me.find('li').click(function paginationClick(){
-            
-                var me = $(this);
-                if(me.hasClass('disabled')){
-                    return;
-                }
-                renderPage($bootpag, parseInt(me.attr('data-lp'), 10));
-            });
-            renderPage($bootpag, settings.page);
-        });
-    }
-
-})(jQuery, window);
+*/
+(function(h){h.fn.bootpag=function(m){function j(e,b){var c,d=Math.floor(b/a.maxVisible)*a.maxVisible,f=e.find("li");a.page=b=0>b?0:b>a.total?a.total:b;f.removeClass("disabled");c=1>b-1?1:a.leaps&&b-1>a.maxVisible?Math.floor((b-1)/a.maxVisible)*a.maxVisible:b-1;f.first().toggleClass("disabled",1===b).attr("data-lp",c).find("a").attr("href",g(c));c=b+1>a.total?a.total:a.leaps&&b+1<a.total-a.maxVisible?d+a.maxVisible+1:b+1;f.last().toggleClass("disabled",b===a.total).attr("data-lp",c).find("a").attr("href",
+g(c));var l=f.filter("[data-lp="+b+"]");if(!l.not(".next,.prev").length){var j=b<=d?-a.maxVisible:0;f.not(".next,.prev").each(function(b){c=b+1+d+j;h(this).attr("data-lp",c).toggle(c<=a.total).find("a").html(c).attr("href",g(c))});l=f.filter("[data-lp="+b+"]")}l.addClass("disabled");k.trigger("page",b);k.data("settings",a)}function g(e){return a.href.replace(a.hrefVariable,e)}var k=this,a=h.extend({total:0,page:1,maxVisible:null,leaps:!0,href:"javascript:void(0);",hrefVariable:"{{number}}",next:"&raquo;",
+prev:"&laquo;"},k.data("settings")||{},m||{});if(0>=a.total)return this;a.maxVisible||(a.maxVisible=a.total);k.data("settings",a);return this.each(function(){var e,b,c=h(this),d=['<ul class="bootpag">'];a.prev&&d.push('<li data-lp="1" class="prev"><a href="'+g(1)+'">'+a.prev+"</a></li>");for(b=1;b<=Math.min(a.total,a.maxVisible);b++)d.push('<li data-lp="'+b+'"><a href="'+g(b)+'">'+b+"</a></li>");a.next&&(b=a.leaps&&a.total>a.maxVisible?Math.min(a.maxVisible+1,a.total):2,d.push('<li data-lp="'+b+'" class="next"><a href="'+
+g(b)+'">'+a.next+"</a></li>"));d.push("</ul>");c.find("ul.bootpag").remove();c.append(d.join("")).addClass("pagination");e=c.find("ul.bootpag");c.find("li").click(function(){var a=h(this);a.hasClass("disabled")||j(e,parseInt(a.attr("data-lp"),10))});j(e,a.page)})}})(jQuery,window);
