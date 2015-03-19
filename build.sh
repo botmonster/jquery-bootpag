413332731a917e8e51aa298ce64b89dbaee21624
botmonster <botmonster@7items.com>
2015-03-19 14:24:25 +0100

    more info about build

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Using closure compiler (https://github.com/google/closure-compiler)
+# http://dl.google.com/closure-compiler/compiler-latest.zip
 java -jar ../compiler-latest/compiler.jar --js lib/jquery.bootpag.js --js_output_file lib/jquery.bootpag.min.js
