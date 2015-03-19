80c66bcb4f24b6975473d9d378325bb5ca81f536
botmonster <botmonster@7items.com>
2015-03-19 19:23:16 +0100

    fix minified 1.0.6 + windows build command

---
+++ build.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+:: Using closure compiler (https://github.com/google/closure-compiler)
+:: http://dl.google.com/closure-compiler/compiler-latest.zip
+java -jar ../compiler-latest/compiler.jar --js lib/jquery.bootpag.js --js_output_file lib/jquery.bootpag.min.js
